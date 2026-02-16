454a95170defd55d34bac563116bcf7c0f0267b1
Tanvi <92842423+tanvi1110@users.noreply.github.com>
2026-02-16 15:10:27 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 16/02/2026, 15:10:26
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
